5333fff6e47d510c0cd5eb0317201293ebabfe0f
Anton Pozdnyakov <Anton.Pozdnyakov.I@gmail.com>
2021-07-29 16:36:02 +0300

    add .env support

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ import asyncio
 import pathlib
 import logging
 from aiohttp import web
+from environs import Env
 
 
 async def archivate(request):
     folder = request.match_info.get('archive_hash')
-    files_dir = pathlib.Path(__file__).parent.joinpath("test_photos").joinpath(folder).absolute()
+    files_dir = pathlib.Path(__file__).parent.joinpath(env('PHOTO_DIR')).joinpath(folder).absolute()
     exceptions = '.' or '..' or None
     if not files_dir.is_dir() or folder == exceptions:
         raise web.HTTPNotFound(
@@ -20,6 +21,8 @@ async def archivate(request):
     await response.prepare(request)
 
     zip_command = ('zip', '-r', '-', '.')
+    file_fragment = env.int('KBYTES') * 1024
+
     process = await asyncio.create_subprocess_exec(
         *zip_command,
         stdout=asyncio.subprocess.PIPE,
@@ -30,19 +33,20 @@ async def archivate(request):
     try:
         while not process.stdout.at_eof():
             logging.debug('Sending archive chunk ...')
-            await response.write(await process.stdout.read(102400))
-            await asyncio.sleep(1)
+            await response.write(await process.stdout.read(file_fragment))
+            await asyncio.sleep(env.int('DELAY'))
 
     except (asyncio.CancelledError, ConnectionResetError, KeyboardInterrupt, SystemExit):
         logging.debug('Download was interrupted')
         await process.communicate()
+        logging.debug(f'Process {process.pid} has been deleted.')
+        raise
+    finally:
         if process.returncode is None or process.returncode:
             try:
                 process.kill()
             except ProcessLookupError:
                 logging.debug(f'Cannot find process {process.pid}. Maybe it has been deleted.')
-        logging.debug(f'Process {process.pid} has been deleted.')
-        raise
 
     return response
 
@@ -54,7 +58,10 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    env = Env()
+    env.read_env()
+
+    logging.basicConfig(level=env.log_level('LOG_LEVEL'))
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
